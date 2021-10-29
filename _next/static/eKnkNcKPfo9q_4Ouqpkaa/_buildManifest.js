4b2b0a3af26cc66d1f9de1409b054090afd9fda8
upsole <80952298+upsole@users.noreply.github.com>
2021-10-29 12:01:18 +0000

    Deploying to gh-pages from @ upsole/fe-mentor-time-tracking-dashboard@d72488662ec11bcb93a44989a48a07015f39cc2c 🚀

---
+++ _next/static/eKnkNcKPfo9q_4Ouqpkaa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/11341f7cddddcf2d.css","static/chunks/pages/index-bb546949ed65c485.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
